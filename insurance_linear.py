963e5fd9a73e07b7aae645e948e18dfe881c0c75
Reinhold B <reinhold.brant@gmx.de>
2021-03-18 13:15:21 +0100

    file name change

---
+++ insurance_linear.py
@@ -0,0 +1,27 @@
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pandas.read_csv("insurance.csv", index_col=0)
+
+x = list(data.axes[0])
+y = list(data["charges"])
+
+LEARNING_RATE = 0.01
+weights = [1, 1]
+for i in range(len(x)):
+    x1, y1 = x[i], y[i]
+    pred = weights[0] * x1 + weights[1]
+    if pred > y1:
+        weights[0] -= pred * LEARNING_RATE
+        weights[1] -= LEARNING_RATE
+    elif pred < y1:
+        weights[0] += pred * LEARNING_RATE
+        weights[1] += LEARNING_RATE
+
+plt.scatter(x, y)
+plt.plot([x for x in range(90)], [x * weights[0] + weights[1] for x in range(90)])
+plt.ylabel("Insurance charges")
+plt.xlabel("Age")
+plt.title(str(weights))
+plt.show()

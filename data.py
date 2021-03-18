ba5a230f79a3877b31afbe6940ac4a08c621f8a3
Reinhold B <reinhold.brant@gmx.de>
2021-03-18 11:01:02 +0100

    cleanup

--- data.py
+++ data.py
@@ -4,14 +4,9 @@ import numpy as np
 
 data = pandas.read_csv("insurance.csv", index_col=0)
 
-print(data.axes)
-
 x = list(data.axes[0])
 y = list(data["charges"])
-print(type(y))
 
-
-# y = ax + b
 LEARNING_RATE = 0.01
 weights = [1, 1]
 for i in range(len(x)):
@@ -26,9 +21,7 @@ for i in range(len(x)):
 
 plt.scatter(x, y)
 plt.plot([x for x in range(100)], [x * weights[0] + weights[1] for x in range(100)])
-
 plt.ylabel("Insurance charges")
 plt.xlabel("Age")
 plt.title(str(weights))
 plt.show()
-# https://github.com/stedy/Machine-Learning-with-R-datasets
